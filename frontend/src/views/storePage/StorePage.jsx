6033a4e61ea18ed2fe0cf8c89b67898e006a57b3
Yohannes Habtemariam <successselam@gmail.com>
2024-04-26 21:06:20 +0200

    App.js and pages

---
+++ frontend/src/views/storePage/StorePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StorePage = () => {
+  return (
+    <main>
+    <article>
+      <h1>Store page</h1>
+    </article>
+  </main>
+  )
+}
+
+export default StorePage
