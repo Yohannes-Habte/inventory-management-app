3c9f48ec88d0141fcbf1588346d3f78dda8aa466
Yohannes Habtemariam <successselam@gmail.com>
2024-04-27 13:43:40 +0200

    Profile page form is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import HomePage from './views/homePage/HomePage';
-import AboutPage from './views/aboutPage/AboutPage';
-import StorePage from './views/storePage/StorePage';
-import ContactPage from './views/contactPage/ContactPage';
-import RegisterPage from './views/registerPage/RegisterPage';
-import LoginPage from './views/loginPage/LoginPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import HomePage from "./views/homePage/HomePage";
+import AboutPage from "./views/aboutPage/AboutPage";
+import StorePage from "./views/storePage/StorePage";
+import ContactPage from "./views/contactPage/ContactPage";
+import RegisterPage from "./views/userPages/registerPage/RegisterPage";
+import LoginPage from "./views/userPages/loginPage/LoginPage";
+import ProfilePage from "./views/userPages/profilePage/ProfilePage";
 
 const App = () => {
   return (
     <div>
-       <Router>
+      <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/store" element={<StorePage />} />
           <Route path="/contact" element={<ContactPage />} />
+
+          {/* User Pages */}
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
-
-     
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
 
         {/* React toastify */}
@@ -40,7 +42,7 @@ const App = () => {
         />
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/views/userPages/registerPage/RegisterPage.jsx
+++ frontend/src/views/userPages/registerPage/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Register from "../../components/user/register/Register";
+import Register from "../../../components/user/register/Register";
 
 const RegisterPage = () => {
   return (
