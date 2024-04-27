8046e0dd8e3acdb841e12645a2085ba892d1dcfe
Yohannes Habtemariam <successselam@gmail.com>
2024-04-27 12:35:01 +0200

    Sign Up Form is done

--- frontend/src/views/registerPage/RegisterPage.jsx
+++ frontend/src/views/registerPage/RegisterPage.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import Register from "../../components/user/register/Register";
 
 const RegisterPage = () => {
   return (
     <main>
-    <article>
-      <h1>Sign Up page</h1>
-    </article>
-  </main>
-  )
-}
+      <article>
+        <h1>Sign Up page</h1>
+        <Register />
+      </article>
+    </main>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
